1eac6707e442bb7a07093dafd5a093c9ac5d90bd
NandDadagha <npdadagha@gmail.com>
2026-02-06 18:25:25 +0530

    Add variables_basics script demonstrating core data types

---
+++ phase-0/variables_basics.py
@@ -0,0 +1,12 @@
+name = "Nand"
+age = 19
+cgpa = 9.5
+is_learning_data_science = True
+print("Name: ", name)
+print("Type of name:", type(name))
+print("Age: ", age)
+print("Type of age:", type(age))
+print("CGPA: ", cgpa)
+print("Type of CGPA:", type(cgpa))
+print("Is learning Data Science: ", is_learning_data_science)
+print("Type of is_learning_data_science:", type(is_learning_data_science))
