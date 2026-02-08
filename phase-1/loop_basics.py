81b7552f02d7e556cd7ab5ec09118336fe91a13d
NandDadagha <npdadagha@gmail.com>
2026-02-08 17:01:48 +0530

    Add loop_basics script demonstrating iteration and manual summation

---
+++ phase-1/loop_basics.py
@@ -0,0 +1,10 @@
+# this script is to demonstrate the basics of loops in Python
+
+scores = [72, 85, 90, 68, 77]
+total = 0
+
+for score in scores:
+    print("Score:", score)
+    total += score
+
+print("Total score:", total)
