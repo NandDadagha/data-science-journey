98c15e7f6f1834c8eb4543462fb19b5da2d06759
NandDadagha <npdadagha@gmail.com>
2026-02-08 17:46:05 +0530

    Add dictionary_basics script demonstrating structured data

---
+++ phase-1/dictionary_basics.py
@@ -0,0 +1,15 @@
+# This script demonstrates bascis of dictionaries in Python.
+
+student = {
+    "name": "Nand",
+    "age": 19,
+    "cgpa": 9.5
+}
+print(student["name"])
+print(student["age"])
+print(student["cgpa"])
+
+student["cgpa"] = 9.7
+student["city"] = "Pune"
+
+print(student)
