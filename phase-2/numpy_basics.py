e525393b35ac2cd817a5c8107f947b21f4164225
NandDadagha <npdadagha@gmail.com>
2026-02-09 16:20:28 +0530

    Imported numpy and demonstrated basic operation with numpy

---
+++ phase-2/numpy_basics.py
@@ -0,0 +1,10 @@
+# This script demonstrates basic operations with NumPy, a powerful library for numerical computing in Python.
+import numpy as np
+
+numbers = np.array([10, 20, 30, 40])
+new_numbers = numbers * 2
+
+print("Original numbers:", numbers)
+print("New numbers:", new_numbers)
+
+print("Type of numbers:", type(numbers))
