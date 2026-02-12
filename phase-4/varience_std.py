50ce88b7acf5a1cc96a8245c20eeb3af638447e2
NandDadagha <npdadagha@gmail.com>
2026-02-12 17:37:36 +0530

    Add varience_std demonstraing Varience and Standard Deviation

---
+++ phase-4/varience_std.py
@@ -0,0 +1,7 @@
+# Variance and standard deviation for a set of scores
+
+import numpy as np
+scores = np.array([72, 85, 90, 60, 88])
+
+print("Variance:",np.var(scores))
+print("Standard Deviation:",np.std(scores))
