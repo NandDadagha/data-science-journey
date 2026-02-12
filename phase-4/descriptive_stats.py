dc08834b9d42054a80c26405a78c42ac28f0a027
NandDadagha <npdadagha@gmail.com>
2026-02-12 17:24:08 +0530

    Add descripive_stats

---
+++ phase-4/descriptive_stats.py
@@ -0,0 +1,10 @@
+# Descriptive statistics for a set of scores
+
+import numpy as np
+scores = np.array([72, 85, 90, 60, 88])
+
+print("Mean:",np.mean(scores))
+print("Median:",np.median(scores))
+print("Min:",np.min(scores))
+print("Max:",np.max(scores))
+print("Standard Deviation:",np.std(scores))
